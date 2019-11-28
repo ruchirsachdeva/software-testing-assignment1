e2a062709f5d04f92e817108ab240bb61c353e39
Ruchir Sachdeva <ruchirsachdeva@gmail.com>
2019-11-29 00:58:29 +0100

    GREEN: Implemented controller to inject GradeService using Dependency injection and to call service.findAll to execute test successfully

--- src/main/java/com/lnu/testing/assignment1/resource/Controller.java
+++ src/main/java/com/lnu/testing/assignment1/resource/Controller.java
@@ -3,9 +3,10 @@ package com.lnu.testing.assignment1.resource;
 
 import com.lnu.testing.assignment1.model.Grade;
 import com.lnu.testing.assignment1.model.GradeType;
+import com.lnu.testing.assignment1.service.GradeService;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+  @Autowired
+  private GradeService service;
+
   @GetMapping("/grades")
   ResponseEntity<List<Grade>> getAllGrades() {
+    service.findAll();
     List<Grade> grades = new ArrayList<>();
     grades.add(new Grade(1L, GradeType.A ,"Software testing"));
     grades.add(new Grade(2L,GradeType.B,"Adaptive and web semantics"));
