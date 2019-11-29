1fb9b3224e8a67ba9196c0ec170467ded809571c
Ruchir Sachdeva <ruchirsachdeva@gmail.com>
2019-11-29 01:44:35 +0100

    RED: Added GradeRepositoryTest to assert that gradeRepository findAll() returns all grades.

---
+++ src/test/java/com/lnu/testing/assignment1/repository/GradeRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.lnu.testing.assignment1.repository;
+
+import static com.lnu.testing.assignment1.util.TestUtil.allGrades;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+
+import com.lnu.testing.assignment1.model.Grade;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GradeRepositoryTest {
+
+  @Autowired private GradeRepository repository;
+
+  @Test
+  public void shouldGetAllGrades(){
+    // given
+    List<Grade> grades = allGrades();
+    // when
+    final List<Grade> response = repository.findAll();
+
+    // then
+    assertThat(response, hasSize(2));
+    assertThat(
+        response,
+        hasItems(
+            allOf(
+                hasProperty("type", is(grades.get(0).getType())),
+                hasProperty("course", is(grades.get(0).getCourse()))),
+            allOf(
+                hasProperty("type", is(grades.get(1).getType())),
+                hasProperty("course", is(grades.get(1).getCourse())))));
+  }
+
+}
