9a1b733b4ff44b6562497c99f5f992515331cee6
Ruchir Sachdeva <ruchirsachdeva@gmail.com>
2019-11-29 01:56:48 +0100

    REFACTOR: Updated Grade entity to be used for ORM using Hibernate JPA for mapping with database table.

--- src/main/java/com/lnu/testing/assignment1/model/Grade.java
+++ src/main/java/com/lnu/testing/assignment1/model/Grade.java
@@ -1,11 +1,27 @@
 package com.lnu.testing.assignment1.model;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Grade {
 
-  private final long id;
-  private final GradeType type;
-  private final String course;
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private long id;
+
+  @Enumerated(EnumType.STRING)
+  private GradeType type;
+
+  private String course;
+
+  protected Grade() {
+    // Only for use by hibernate.
+  }
 
   public Grade(long id, GradeType gradeType, String course) {
     this.id = id;
@@ -24,8 +40,4 @@ public class Grade {
   public String getCourse() {
     return course;
   }
-
-
-
-
 }
