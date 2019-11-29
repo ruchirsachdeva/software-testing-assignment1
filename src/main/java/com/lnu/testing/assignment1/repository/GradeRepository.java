9c4c02674185f4d06d82043efe984c8f5691ad73
Ruchir Sachdeva <ruchirsachdeva@gmail.com>
2019-11-29 01:27:37 +0100

    GREEN: Added GradeRepository as a Repository Spring bean, Implemented service to inject GradeRepository using Dependency Injection and to call repository.findAll to execute test successfully

--- src/main/java/com/lnu/testing/assignment1/repository/GradeRepository.java
+++ src/main/java/com/lnu/testing/assignment1/repository/GradeRepository.java
@@ -2,7 +2,9 @@ package com.lnu.testing.assignment1.repository;
 
 import com.lnu.testing.assignment1.model.Grade;
 import java.util.List;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class GradeRepository {
 
   public List<Grade> findAll() {
--- src/main/java/com/lnu/testing/assignment1/service/GradeService.java
+++ src/main/java/com/lnu/testing/assignment1/service/GradeService.java
@@ -2,14 +2,19 @@ package com.lnu.testing.assignment1.service;
 
 import com.lnu.testing.assignment1.model.Grade;
 import com.lnu.testing.assignment1.model.GradeType;
+import com.lnu.testing.assignment1.repository.GradeRepository;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GradeService {
 
+  @Autowired private GradeRepository repository;
+
   public List<Grade> findAll() {
+    repository.findAll();
     List<Grade> grades = new ArrayList<>();
     grades.add(new Grade(1L, GradeType.A, "Software testing"));
     grades.add(new Grade(2L, GradeType.B, "Adaptive and web semantics"));
